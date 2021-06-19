0764ad2289b86707b04e7676c64e31d5040580c0
Alexander Nguyen <41305327+nguyenalex214@users.noreply.github.com>
2021-06-18 19:26:59 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,43 @@ import "./index.css";
 /*
  Your solution here
  */
+const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const errors = validateForm(formData);
+    if(!errors) {
+        const errorDiv = document.createElement("div");
+        errorDiv.classList.add("error");
+        errorDiv.setAttribute("id", "searchError");
+        const errorMsg = "Please enter a search term";
+        const content = 
+        `<div class="error" id="searchError">${errorMsg}</div>`;
+
+        errorDiv.innerHTML = content;
+        document.querySelector("form").appendChild(errorDiv);
+    } 
+
+    const findArticle = document.querySelectorAll("article");
+    findArticle.forEach((articles) => {
+        if(!articles.innerHTML.toLowerCase().includes(formData.get("searchTerm").toLowerCase())) {
+            articles.classList.add("hidden");
+        } else {
+            articles.classList.remove("hidden");
+        }
+    })
+}
+
+function validateExists(value) {
+    return value && value.trim();
+}
+
+const validateForm = (formData) => {
+    return validateExists(formData.get("searchTerm"));
+}
+
+const main = () => {
+    const form = document.querySelector("#searchForm");
+    form.addEventListener("submit", formSubmitHandler);
+}
+
+window.addEventListener("DOMContentLoaded", main)
